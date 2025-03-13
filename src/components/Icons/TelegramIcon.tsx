827174db753ecaf21c9fe635c7d3c8b42a7e1878
giorgionetg <giorgionetg@gmail.com>
2025-03-13 06:37:55 +0100

    icons

--- src/components/Icons/TelegramIcon.tsx
+++ src/components/Icons/TelegramIcon.tsx
@@ -1,7 +1,15 @@
 import { SocialIcon } from 'react-social-icons';
+import useNavbarStore from '@/components/Navbar/useNavbarStore';
 
 export default function TelegramIcon(props: { url: string }) {
+  const { isMobile } = useNavbarStore();
+  const classes = isMobile ? 'ftso-icon-set-mobile' : 'ftso-icon-set';
   return (
-    <SocialIcon label='Ftso.eu' className='ftso-icon-set' url={props.url} />
+    <SocialIcon
+      label='Ftso.eu'
+      className={classes}
+      style={{}}
+      url={props.url}
+    />
   );
 }
--- src/components/Icons/YoutubeIcon.tsx
+++ src/components/Icons/YoutubeIcon.tsx
@@ -1,10 +1,13 @@
 import { SocialIcon } from 'react-social-icons';
+import useNavbarStore from '@/components/Navbar/useNavbarStore';
 
 export default function YoutubeIcon(props: { url: string }) {
+  const { isMobile } = useNavbarStore();
+  const classes = isMobile ? 'ftso-icon-set-mobile' : 'ftso-icon-set';
   return (
     <SocialIcon
       label='Ftso.eu'
-      className='ftso-icon-set'
+      className={classes}
       style={{}}
       url={props.url}
     />
