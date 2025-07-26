9b11a74c49784c50954a21ba26b2fd2a5cabfce0
giorgionetg <giorgionetg@gmail.com>
2025-07-26 08:28:37 +0200

    better Common title

--- src/components/Hero/Common.tsx
+++ src/components/Hero/Common.tsx
@@ -4,7 +4,9 @@ export default function Common(props: { title: string }) {
   return (
     <div className='flex flex-auto w-full h-[250px] overflow-hidden bg-hero-pattern-news bg-cover bg-center bg-opacity-100 p-0 mx-auto'>
       <MainWrapper isNavbar>
-        <h1 className='text-white dark:text-foreground'>{props.title}</h1>
+        <h1 className='text-white text-[25px] font-bold dark:text-foreground'>
+          {props.title}
+        </h1>
       </MainWrapper>
     </div>
   );
